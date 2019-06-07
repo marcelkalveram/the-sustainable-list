517b9993e47e971ebd41cb20584945f62ebabaa8
Marcel Kalveram <marcel.kalveram@googlemail.com>
2019-06-07 13:07:39 +0200

    add certification icons

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Pane, Link, Heading, Text } from 'evergreen-ui';
+import {
+  Pane,
+  Link,
+  Heading,
+  Text,
+  Tooltip,
+  Position,
+  Icon,
+} from 'evergreen-ui';
+import ReactTooltip from 'react-tooltip';
+import { majorScale } from 'evergreen-ui/commonjs/scales';
+import * as images from '../images';
 
 export function Brands(props) {
   const filtered = props.brands.filter(brand => {
@@ -85,6 +96,123 @@ export function Brands(props) {
                   .map(style => style.fields.title)
                   .join(', ')}
               </Text>
+            )}
+            {brand.fields.certificates && (
+              <Pane marginTop={majorScale(1)}>
+                <ReactTooltip effect="solid" />
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('global-recycled-standard-grs') && [
+                  <img
+                    data-tip="Global Recycled Standard"
+                    style={{
+                      marginRight: majorScale(1),
+                    }}
+                    width={20}
+                    height={20}
+                    src={images.badgeGrs}
+                  />,
+                ]}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('fsc-certified-rubber') && (
+                  <img
+                    data-tip="FSC Certified Rubber"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeFsc}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('fair-wear-foundation') && (
+                  <img
+                    data-tip="Fair Wear Foundation"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeFwf}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('oekotex-standard-100') && (
+                  <img
+                    data-tip="Oekotex Standard 100"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeOtx}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('soil-association') && (
+                  <img
+                    data-tip="Soil Association"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeSas}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('global-organic-textile-standard-gots') && (
+                  <img
+                    data-tip="Global Organic Textile Standard"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeGot}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('peta-approved-vegan') && (
+                  <img
+                    data-tip="Peta Approved Vegan"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgePta}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('1-for-the-planet') && (
+                  <img
+                    data-tip="1% For The Planet"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeOne}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('certified-organic-cotton') && (
+                  <img
+                    data-tip="Certified Organic Cotton"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeCoc}
+                  />
+                )}
+                {brand.fields.certificates
+                  .map(cert => cert.fields.slug)
+                  .includes('supports-environmental-organisation') && (
+                  <img
+                    data-tip="Supports Environmental Organizations"
+                    style={{ marginRight: majorScale(1) }}
+                    width={20}
+                    height={20}
+                    src={images.badgeEnv}
+                  />
+                )}
+              </Pane>
             )}
             {/* {brand.fields.clothingTypes && (
               <Text size={300}>
---
+++ src/images/index.js
@@ -0,0 +1,23 @@
+import badgeGrs from './badges-grs.png';
+import badgeFsc from './badges-fsc.jpg';
+import badgeFwf from './badges-fwf.png';
+import badgeOtx from './badges-otx.png';
+import badgeSas from './badges-sas.png';
+import badgeGot from './badges-got.png';
+import badgePta from './badges-pta.png';
+import badgeOne from './badges-one.png';
+import badgeCoc from './badges-coc.png';
+import badgeEnv from './badges-env.png';
+
+export {
+  badgeGrs,
+  badgeFsc,
+  badgeFwf,
+  badgeOtx,
+  badgeSas,
+  badgeGot,
+  badgePta,
+  badgeOne,
+  badgeCoc,
+  badgeEnv,
+};
