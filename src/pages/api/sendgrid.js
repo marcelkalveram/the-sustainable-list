0a8d6a9c887e7d46a0be91e2207eec4b5c4766f6
Marcel Kalveram <marcel.kalveram@googlemail.com>
2020-02-08 16:17:07 +0100

    add errors in contact api

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -2,6 +2,15 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendMail = ({ name, email, message }) => {
+  if (!process.env.SENDGRID_API_KEY) {
+    throw new Error('now API key defined');
+  }
+  if (!email || !message) {
+    throw new Error('insufficient form data');
+  }
+  if (!name) {
+    name = 'unknown';
+  }
   const msg = {
     to: 'marcel.kalveram@googlemail.com',
     from: 'mail@thesustainablelist.com',
