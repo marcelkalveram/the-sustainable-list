a062214cabb32311852972deff4ec42314c097ee
Marcel Kalveram <marcel.kalveram@googlemail.com>
2024-07-31 08:12:00 +0200

    chore: add TS to toggle fn

--- src/helpers/toggle.ts
+++ src/helpers/toggle.ts
@@ -1,15 +1,19 @@
-export const toggleHiddenFn = (hidden, setHidden) => (toggledItem) => {
-  let newHidden = hidden;
-  if (!newHidden.includes(toggledItem)) {
-    newHidden.push(toggledItem);
-  } else {
-    newHidden = newHidden.filter((item) => toggledItem !== item);
-  }
-  setHidden([...newHidden]);
-};
+import type { CriteriaMapFlattened } from "types";
+
+export const toggleHiddenFn =
+  (hidden: string[], setHidden: Function) => (toggledItem: string) => {
+    let newHidden = hidden;
+    if (!newHidden.includes(toggledItem)) {
+      newHidden.push(toggledItem);
+    } else {
+      newHidden = newHidden.filter((item) => toggledItem !== item);
+    }
+    setHidden([...newHidden]);
+  };
 
 export const toggleFilterFn =
-  (selected, setSelected) => (toggledItem, type) => {
+  (selected: CriteriaMapFlattened, setSelected: Function) =>
+  (toggledItem: string, type: string) => {
     const newSelected = selected;
     if (newSelected[type].includes(toggledItem)) {
       newSelected[type] = newSelected[type].filter(
